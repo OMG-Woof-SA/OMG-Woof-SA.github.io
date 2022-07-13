c942380152cdaa440698cc8c6facc57678f10bcf
OMG-Woof-SA <108983528+OMG-Woof-SA@users.noreply.github.com>
2022-07-13 14:01:38 +0500

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -62,7 +62,7 @@ const RESOURCES = {
 "assets/assets/watch_tag.png": "d8d5c06ae0a382faf16da7aba64b833a",
 "assets/FontManifest.json": "7aaf3996738086bbd796613e14ef9e45",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"assets/NOTICES": "9e637bc280da3b610daabf94f9c9e584",
+"assets/NOTICES": "36d31804f03c5380d62a014f60789a8d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -91,9 +91,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1c08988badd40ac71924f3652aa2a218",
-"/": "1c08988badd40ac71924f3652aa2a218",
-"main.dart.js": "15ecda9896ab8e57de68a53d45851d84",
+"index.html": "a4024a78b69e178fc2db4a488b059bbf",
+"/": "a4024a78b69e178fc2db4a488b059bbf",
+"main.dart.js": "189717751248347670b47cd471d92c1c",
 "manifest.json": "076c70aea5fdc4ac0531ee70b062b5b0",
 "version.json": "f2b9345c1d8b293098ca5a819db04d96"
 };
